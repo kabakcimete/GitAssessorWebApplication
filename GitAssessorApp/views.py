cd8041fbff707c5265c5ade287ed98f1e552e9ab
dogukancatal <dogukancatal9807@gmail.com>
2022-05-02 14:49:04 +0300

    checkboxes according to users

--- GitAssessorApp/views.py
+++ GitAssessorApp/views.py
@@ -1,13 +1,53 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render,HttpResponse,redirect
 from .forms import *
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
+from GitAssessorApp.models import Checkedrules
+
 
 # Create your views here.
 
 def home(request):
-   return render(request,"homepage.html")
+	rules={
+		"does every contributor have commit":"1",
+		"does repository have read_me file":"2",
+		"does every commit has comment":"3",
+		"does every contributor have feature branch":"4"
+	}
+	if request.user.is_authenticated:
+		loggeduser=request.user
+		#return HttpResponse(request.user.id)
+		print(loggeduser)
+		userObj=Checkedrules.objects.get(user=loggeduser)
+		if Checkedrules.objects.filter(user=loggeduser).exists():
+			check_list=list(userObj.checkedrules.split(","))
+			print(check_list)
+
+		if request.method=="POST":
+			
+			
+				#return render(request,"assess.html")
+			chkbox=request.POST.get("chkbox")
+			userObj=Checkedrules.objects.get(user=loggeduser)
+			# return HttpResponse(userObj)
+			if Checkedrules.objects.filter(user=loggeduser).exists():
+				userObj.checkedrules=chkbox
+				userObj.save()
+				print(userObj.checkedrules)
+				return redirect("../home")
+			else:
+				savedata=Checkedrules()
+				savedata.user=loggeduser
+				savedata.checkedrules=chkbox
+				savedata.save()
+				return redirect("../home")
+
+		return render(request,"homepage.html",{"user_rules":check_list,"rules":rules})
+	else:
+		check_list=[]
+		return render(request,"homepage.html",{"user_rules":check_list,"rules":rules})
 
 
 def assess(request):
