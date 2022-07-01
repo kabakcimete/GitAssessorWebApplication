47d6514253d3ee1ce21641f6b4b5ea9efba6655a
dogukancatal <dogukancatal9807@gmail.com>
2022-07-02 01:01:15 +0300

    register problem solved

--- GitAssessorApp/urls.py
+++ GitAssessorApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from .views import *
 
 urlpatterns = [
-    path("home/",home),
+    path("",home),
     path("assess/",assess),
     path("register/",register_request),
     path("login/",login_request),
